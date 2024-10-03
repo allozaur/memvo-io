ba5ed457b61e59002377d27a3fbc10c79831e21c
Aleksander Grygier <aleksander.grygier@resider.pl>
2024-10-04 00:38:05 +0200

    feat: Recording audio feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,99 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let audioUrl = $state('');
+
+	/** @type {BlobPart[]} */
+	let chunks = [];
+
+	let isRecording = $state(false);
+
+	/** @type {MediaRecorder | null} */
+	let mediaRecorder = null;
+
+	let recordingName = $state('');
+
+	async function startRecording() {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+			mediaRecorder = new MediaRecorder(stream);
+
+			mediaRecorder.ondataavailable = (event) => {
+				chunks.push(event.data);
+			};
+
+			mediaRecorder.onstop = () => {
+				const audioBlob = new Blob(chunks, { type: 'audio/webm' });
+				chunks = [];
+				audioUrl = URL.createObjectURL(audioBlob);
+			};
+
+			mediaRecorder.start();
+			isRecording = true;
+		} catch (error) {
+			console.error('Error starting recording:', error);
+		}
+	}
+
+	function stopRecording() {
+		if (mediaRecorder && isRecording) {
+			mediaRecorder.stop();
+			isRecording = false;
+		}
+
+		recordingName = `recording-${new Date().toISOString()}`;
+	}
+</script>
+
+<main>
+	{#if isRecording}
+		<button class="stop" onclick={stopRecording}> ⏹</button>
+	{:else}
+		<button class="start" onclick={startRecording}>⏺</button>
+	{/if}
+
+	{#if audioUrl}
+		<div class="recording">
+			<audio controls src={audioUrl}></audio>
+
+			<a href={audioUrl} download="{recordingName}.webm">Download Recording</a>
+		</div>
+	{/if}
+</main>
+
+<style>
+	main {
+		display: grid;
+		place-content: center;
+		gap: 2rem;
+		place-items: center;
+		height: 100vh;
+	}
+
+	button {
+		background: none;
+		border: none;
+		border-radius: 100%;
+		font-size: 3rem;
+		height: 3.5rem;
+		width: 3.5rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.start {
+		background: #f0f0f0;
+		color: red;
+	}
+
+	.stop {
+		background: red;
+		color: white;
+	}
+
+	.recording {
+		display: grid;
+		place-content: center;
+		place-items: center;
+		gap: 1rem;
+	}
+</style>

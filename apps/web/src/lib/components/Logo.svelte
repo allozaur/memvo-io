25c292e6569cf3ef792dec289757919a16f29f56
Aleksander Grygier <aleksander.grygier@resider.pl>
2024-10-04 12:58:31 +0200

    refactor: Import app

---
+++ apps/web/src/lib/components/Logo.svelte
@@ -0,0 +1,33 @@
+<script>
+	const icons = import.meta.glob('../../../static/svg/brand/*.svg', {
+		query: '?raw',
+		import: 'default',
+		eager: true
+	});
+</script>
+
+<div class="logo" data-testid="icon" on:click on:keydown role="none">
+	{@html icons[`../../../static/svg/brand/memvo-logo.svg`]}
+</div>
+
+<!-- <style>
+	text {
+		fill: var(--c-text) !important;
+	}
+</style> -->
+
+<style lang="postcss">
+	.logo {
+		display: contents;
+		font-size: var(--size, 2rem);
+	}
+
+	.logo :global(svg) {
+		height: 1em;
+		width: auto;
+	}
+
+	.logo :global(path) {
+		fill: var(--c-text);
+	}
+</style>
---
+++ apps/web/src/lib/components/RecordingTile.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Button from './Button.svelte';
+
+	interface RecordingTileProps {
+		name: string;
+		url: string;
+		recordings: { name: string; url: string }[];
+	}
+
+	let { name, url, recordings }: RecordingTileProps = $props();
+</script>
+
+<div class="recording-tile">
+	<h3>{name}</h3>
+
+	<div>
+		<audio controls src={url}></audio>
+	</div>
+
+	<Button download="{name}.webm" href={url} label="Download"></Button>
+
+	<Button
+		kind="secondary"
+		onclick={() => {
+			if (confirm('Do you want to delete this recording?')) {
+				recordings.splice(
+					recordings.findIndex((r) => r.name === name),
+					1
+				);
+			}
+		}}
+		label="Delete"
+	/>
+</div>
+
+<style>
+	.recording-tile {
+		border: 1px solid #ccc;
+		border-radius: 1.25rem;
+		background: var(--c-surface);
+		color: var(--c-text);
+		padding: 1rem;
+		display: grid;
+		gap: 1rem;
+		align-items: start;
+	}
+</style>

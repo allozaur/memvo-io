aab22e239103bba9f20a469099b56cde7670e054
Aleksander Grygier <aleksander.grygier@resider.pl>
2024-10-09 15:46:53 +0200

    test: Update integration test

--- apps/web/tests/test.js
+++ apps/web/tests/test.js
@@ -2,5 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('.logo')).toBeVisible();
+	await expect(page.locator('.hero h1')).toBeVisible();
+	await expect(page.locator('.hero p')).toBeVisible();
+	await expect(page.locator('.hero .button')).toBeVisible();
 });

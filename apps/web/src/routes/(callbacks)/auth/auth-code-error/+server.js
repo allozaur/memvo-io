f1924770ea17f78680c6ffa79926deb0ee284129
Aleksander Grygier <aleksander.grygier@resider.pl>
2024-10-09 15:43:34 +0200

    fix: Remove unneeded code

--- apps/web/src/routes/(callbacks)/auth/auth-code-error/+server.js
+++
@@ -1,7 +0,0 @@
-import { t } from '@resider/i18n';
-import { redirect } from '@sveltejs/kit';
-import { get } from 'svelte/store';
-
-export function GET() {
-  redirect(303, `/${get(t)('slugs.sign-in')}`);
-}
